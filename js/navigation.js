3baf32a2902fd016b498d77cdff3ed01f082b72a
Rithrian <87026495+Rithrian@users.noreply.github.com>
2025-09-15 22:38:00 -0500

    initial commit

---
+++ js/navigation.js
@@ -0,0 +1,36 @@
+[
+  {
+    label: "Events",
+    href: "index.html",
+    notice: {
+      text: "Are you having trouble finding an internship? Don't know where to start? Join us soon at the ",
+      link: {
+        href: "https://ozsecurity.org/career-panel25.html",
+        label: "School of Computing's cybersecurity careers panel",
+      },
+    },
+  },
+  {
+    label: "Join Us",
+    href: "recruitment.html",
+  },
+  {
+    label: "Gallery",
+    href: "gallery.html",
+  },
+  {
+    label: "Partnerships",
+    href: "partnerships.html",
+  },
+  {
+    label: "About Us",
+    href: "about.html",
+    notice: {
+      text: "Curious who built this site or need a site made for your student organization? Check out the ",
+      link: {
+        href: "https://www.linkedin.com/in/jason-kurtz-246940233",
+        label: "developer's page",
+      },
+    },
+  },
+];
